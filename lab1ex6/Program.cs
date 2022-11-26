3a4adb60ae687028be95206e9e6901f4e9d7e9ad
Borze Andrei <borzeandrew@gmail.com>
2022-11-26 22:08:16 +0200

    Add project files.

---
+++ lab1ex6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lab1ex6
+{
+    /*Se citesc trei numere de la tastatura, x,y,z. Scrieti un program care va afisa cele trei valori in ordine descrescatoare.*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Acest program va afisa in ordine descrescatoare cele 3 numere citite de la tastatura. \n");
+
+            Console.WriteLine("x:");
+            var x = int.Parse(Console.ReadLine());
+            Console.WriteLine("y:");
+            var y = int.Parse(Console.ReadLine());
+            Console.WriteLine("z:");
+            var z = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ordinea descrescatoare: " + z +" "+ y + " " + x);
+        }
+    }
+}
